a1af80ebbad0c0e9b2499a955c0fa2748223eb6b
computershoes <dietterichmax@gmail.com>
2022-01-24 00:17:26 -0500

    Update File

--- week-03/scripts/roulette_dealer_finder_by_time_and_game.sh
+++ week-03/scripts/roulette_dealer_finder_by_time_and_game.sh
@@ -1,24 +1,20 @@
 #!/bin/bash/
 
-# Blackjack
 Blackjack()
 {
 	awk -F" " '{print $1 " " $2 " " $3 " " $4}'
 }
-# Roulette
 Roulette()
 {
 	awk -F" " '{print $1 " " $2 " " $5 " " $6}'
 }
-# Texas Hold Em
 Texas_Hold_Em()
 {
 	awk -F" " '{print $1 " " $2 " " $7 " " $8}'
 }
 
-# Search for a roulette dealer on the schedule using time (00:00:00 AM/PM), date (MMDD), and casino game.
+# Search for a roulette dealer on the schedule using time (00:00:00 AM/PM), date (MMDD), and casino game (B, R, T).
 # $1 = time $2 = date $3 = casino game
-# Possible inputs for $3 are: B, R, T
 
 grep "$1" $2_Dealer_schedule |
 
